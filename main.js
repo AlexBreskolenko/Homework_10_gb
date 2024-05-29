6207fd32d358ad8e5dfc0b0f0acd6759eb2539f7
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-05-29 23:53:09 +0300

    code main.js completed

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const menuActive = document.querySelector(".pop-up__menu");
+const burger = document.querySelector(".header-section__btn_butter");
 
+function toggleMenu() {
+  menuActive.classList.toggle("hidden");
+}
+
+burger.addEventListener("click", toggleMenu);
